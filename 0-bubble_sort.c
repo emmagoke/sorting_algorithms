bef2b76edae9d9ec9bb8370851ec49c613e4f0b6
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-01-24 09:21:11 -0800

    Made corrections

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -13,16 +13,16 @@ void bubble_sort(int *array, size_t size)
 
 	if (size < 2)
 		return;
-	for (i = 0; i < size; i++)
+	for (i = 1; i < size; i++)
 	{
 		swap = 0;
-		for (j = 1; j < (size - i); j++)
+		for (j = 0; j < (size - i); j++)
 		{
-			if (array[j - 1] > array[j])
+			if (array[j] > array[j + 1])
 			{
-				temp = array[j - 1];
-				array[j - 1] = array[j];
-				array[j] = temp;
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
 				print_array(array, size);
 				swap = 1;
 			}
